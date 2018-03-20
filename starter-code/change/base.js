f1b46dcf4e2af36c7b2252d942cfa7faab7bbb7b
Roselou <elizabethroselou@gmail.com>
2018-03-20 12:33:54 -0700

    answers to change

--- starter-code/change/base.js
+++ starter-code/change/base.js
@@ -4,4 +4,17 @@ $(document).ready(function(){
 
   $("#time").text( Date.now() );
 
-})
+function handleChange(event) {
+	var $left = $('#left');
+	var $right = $('#right');
+	var leftVal = parseInt($left.val());
+	var rightVal = parseInt($right.val());
+	$('#total').val(leftVal + rightVal)
+}
+
+$('.add').on('change', handleChange)
+
+// $('.number').on(change(function(){
+// 	$('#total') = $('#left') + $('#right');
+// })
+// })
